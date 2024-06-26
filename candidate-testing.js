10ed1cb5ff5103ee27c3821cae6a7965277f672a
rswade84 <renaldowade@gmail.com>
2024-06-26 12:50:37 -0500

    Part #2 Graded Assigment #1

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,10 +10,20 @@ let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+// Stores All Questions & Answers, its an ARRAY of STRINGS.
+let questions = [
+  'Who was the first American woman in space? ',
+  'True or false: 5 kilometer == 5000 meters? ',
+  '(5 + 3)/2 * 10 = ? ',
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  'What is the minimum crew size for the ISS? ',
+];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+
+// This Stores candidateAnswers and will be initialized later
 let candidateAnswers;
 
+// This Askes Candidate's name
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question('Enter your name:');
@@ -21,23 +31,46 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
+// This functions Grades the Quiz
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-  if (candidateAnswer === 'Sally Ride') {
-    console.log(`That's correct ${candidateName}! Great job!`);
-  } else {
-    console.log(
-      `Sorry, ${candidateName}, that's not correct. The correct answer is "${correctAnswer}`,
-    );
-  }
+  let correctCount = 0;
 
-  let grade; //TODO 3.2 use this variable to calculate the candidates score.
+  // This loops through each question.
+  for (let i = 0; i < questions.length; i++) {
+    // Compares Questions to Answers, converts to lower caser prior.
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      correctCount++;
+    }
+    // Shows each questions, answer, and correct answer
+    console.log(`Question ${i + 1}: ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+  }
+  // Calulates the grade into a percentage
+  let grade = (correctCount / questions.length) * 100;
+
+  // This shows Overall Grade
+  console.log(
+    `Overall Grade: ${grade}% (${correctCount} of ${questions.length} responses correct)`,
+  );
+
+  // Decides if user passes or fails.
+  if (grade >= 80) {
+    console.log(`Your Results: PASSED!`);
+  } else {
+    console.log(`Your Resultss: FAILED`);
+  }
 
   return grade;
 }
+
+// This function runs the program
 
 function runProgram() {
   askForName();
